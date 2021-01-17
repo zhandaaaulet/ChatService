3a7f814c435ad27fbc669c32995a03d2c89026f3
zhandaaaulet <zh.myrzatayev@gmail.com>
2021-01-17 22:49:17 +0600

    first commit

---
+++ ChatProject/ChatRoom.cs
@@ -0,0 +1,49 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatProject
+{
+    public class ChatRoom
+    {
+        private readonly ConcurrentDictionary<string, IServerStreamWriter<Message>> users = new ConcurrentDictionary<string, IServerStreamWriter<Message>>();
+
+        public void Join(string name, IServerStreamWriter<Message> response) => users.TryAdd(name, response);
+
+        public void Remove(string name) => users.TryRemove(name, out _);
+
+        public async Task BroadcastMessageAsync(Message message) => await BroadcastMessage(message);
+
+        private async Task BroadcastMessage(Message message)
+        {
+            foreach (var user in users.Where(x => x.Key != message.User))
+            {
+                var item = await SendMessageToSubscriber(user, message);
+                if (item != null)
+                {
+                    Remove(item?.Key);
+                }
+            }
+        }
+
+        private async Task<KeyValuePair<string, IServerStreamWriter<Message>>?> SendMessageToSubscriber(KeyValuePair<string,
+            IServerStreamWriter<Message>> user,
+            Message message)
+        {
+            try
+            {
+                await user.Value.WriteAsync(message);
+                return null;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return user;
+            }
+
+        }
+    }
+}
---
+++ ChatProject/Services/ChattingService.cs
@@ -0,0 +1,36 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatProject.Services
+{
+    public class ChattingService : ChatService.ChatServiceBase
+    {
+        private readonly ChatRoom _chatRoomService;
+
+        public ChattingService(ChatRoom chatRoomService)
+        {
+            _chatRoomService = chatRoomService;
+        }
+
+        public override async Task Join(IAsyncStreamReader<Message> requestStream,
+            IServerStreamWriter<Message> responseStream,
+            ServerCallContext context)
+        {
+            if (!await requestStream.MoveNext()) return;
+
+            do
+            {
+                _chatRoomService.Join(requestStream.Current.User, responseStream);
+                await _chatRoomService.BroadcastMessageAsync(requestStream.Current);
+            } while (await requestStream.MoveNext());
+
+            _chatRoomService.Remove(context.Peer);
+
+
+
+        }
+    }
+}
